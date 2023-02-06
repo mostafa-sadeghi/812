e27a6aa94018c0b8bdaf3b03eaa1f86948ba18d5
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-06 17:58:49 +0330

    sadeghi

---
+++ 3.py
@@ -0,0 +1,53 @@
+# x = 1
+# name1 = 'karen'
+# _name = 'ayrana'
+# name_one = 'amirhossein'
+
+# اسم متغیر نمی تواند با عدد شروع شود.
+
+# یعنی کامنت
+
+# number_one = 1
+# print(type(number))
+
+# number_two = 1.5
+# print(type(number))
+
+# result = number_one + number_two
+# print("result is :", result)
+
+# str data type   نوع داده رشته
+# message = "he said \"do some thing\""
+# print(type(message))
+# message = 'he said "do some thing"'
+# print(type(message))
+# print(message)
+
+
+# message = 'he said you "could\'nt do some thing"'
+# print(message)
+
+# message = "hello every body. welcome to our python class.\nin this course we are going to learn every things about python from zero to hero."
+# print(message)
+# message = '''hello every body. welcome to our python class.
+# in this course we are going to learn every things about python from zero to hero.'''
+# print(message)
+# message = """hello every body. welcome to our python class.
+# in this course we are going to learn every things about python from zero to hero."""
+# print(message)
+# message = '''he said you "could'nt do some thing"'''
+# print(message)
+
+name = "artin"
+family = "rezaei"
+
+message = "hello" + " "+name+" " + family
+print(message)
+
+
+#  تمام کدهای بالا را مرور کنید و همینطور برنامه زیر را بنویسید
+# name = اسم خودتون
+# family = فامیلی خودتون
+# age = سن خودتون
+# message = my name is  -------   and i am ---- years old
+# print(message)
