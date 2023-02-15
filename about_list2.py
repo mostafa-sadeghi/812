864196ec9829aaa1802ef70e89291ff31ec38a8e
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-15 17:59:13 +0330

    first commit

---
+++ about_list2.py
@@ -0,0 +1,33 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# result = numbers[0] + numbers[1] +
+
+result = sum(numbers)
+print("result is:", result)
+
+# slice  برش
+# عددهای فرد در لیست بالا
+odd_numbers = numbers[0:-1:2]
+print("odd numbers are:", odd_numbers)
+
+
+# exercise 1:
+#    اعداد زوج را لیست بالا پرینت کنید
+
+
+first_three_nums = numbers[0:3]
+print("first_three_nums", first_three_nums)
+
+characters = ['a', 'b', 'c', 'd', 'e', 'f']
+
+#  ['a','b','c']
+print(characters[0:3])
+
+# ['b', 'c']
+# ['c', 'd', 'e']
+
+
+# ٍ exer 2
+#  [ 'e', 'f']
+# ['d', 'e']
+# ['b', 'c', 'd', 'e']
