04f697ab7a91593e598241050c970d86c0f652ce
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-20 18:02:24 +0330

    sadeghi

---
+++ list3.py
@@ -0,0 +1,30 @@
+# shopping_list = []
+# shopping_list.append('egg')
+# shopping_list.append('banana')
+# shopping_list.append('apple')
+
+# print("shopping list items are:", shopping_list)
+
+# del shopping_list[0]
+# print("shopping list after removing first item:", shopping_list)
+
+# shopping_list.remove('apple')
+# print("shopping list after removing apple", shopping_list)
+
+
+# star = '*'
+# print('*' * 10)
+
+# str1 = "123"
+# str2 = "abc"
+
+# print(str1 + str2)
+
+# n = [1,2,3]
+
+# print(n * 3)
+
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+print(list1 + list2)
---
+++ tuple1.py
@@ -0,0 +1,8 @@
+numbers = (1, 2, 3, 4, 5)
+print(type(numbers))
+
+print(numbers[0])
+print(numbers[:3])
+# print(sum(numbers))
+print(numbers[1::2])
+# numbers[0] = 1000 # TypeError
