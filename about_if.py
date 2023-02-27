65100f5b88ad404fbe7cbe81eecdd0df8b4808d9
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-27 17:56:53 +0330

    sadeghi

---
+++ about_if.py
@@ -0,0 +1,31 @@
+# a = 4
+# b = 4
+
+# if a >= b:
+#     print("a is greater than or equal to b.")
+
+# if a < b:
+#     print("a is lower than b.")
+
+
+# if a == b:
+#     print("a is equal to b.")
+
+
+a = 4
+b = 5
+
+if a != b:
+    print("a is not equal to b.")
+
+if not a == b:
+    print("a is not equal to b.")
+
+
+# exercise 1: برنامه ای بنویسید که سه عدد را از ورودی دریافت نماید
+# و اگر عدد اول از عدد دوم و نیز عدد سوم بزرگتر بود پیغام زیر را نمایش دهد
+# first number is greater than second and third one.
+# اگر عدد اول از عدد دوم بزرگتر و از عدد سوم کوچکتر بود پیغام زیر را نمایش دهد
+# first number is greater than second number but is lower than third one.
+# اگر عدد اول از عدد دوم کوچکتر ولی از عدد سوم بزرگتر باشد، پیغام زیر را نمایش دهد
+# first number is greater than third number but is lower than second one.
---
+++ review1.py
@@ -0,0 +1,28 @@
+# print('hello')
+# print("1+2*3")
+# print(1 + 2 * 3)
+
+# score = 0
+# print("score :", score)
+
+# score = score + 1
+# print("score :", score)
+
+# score += 1
+# print("new score:", score)
+
+# score = score - 1
+# print("new score:", score)
+# score -= 1
+# print("new score:", score)
+
+# print("hello\neverybody")
+# print("hello\tevrybody")
+# print("hello\reverybody.")
+# print("hello everybody.\rwelcome to our python class.\rwelcome to our pygame")
+
+print(4 / 2)
+print(4 % 2)
+print(5 % 2)
+print(3/2)
+print(3//2)
