15aee3337763cf989bdf0d0ec7520650bc404a28
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-01 18:02:58 +0330

    sadeghi

--- about_if.py
+++ about_if.py
@@ -12,14 +12,14 @@
 #     print("a is equal to b.")
 
 
-a = 4
-b = 5
+# a = 4
+# b = 5
 
-if a != b:
-    print("a is not equal to b.")
+# if a != b:
+#     print("a is not equal to b.")
 
-if not a == b:
-    print("a is not equal to b.")
+# if not a == b:
+#     print("a is not equal to b.")
 
 
 # exercise 1: برنامه ای بنویسید که سه عدد را از ورودی دریافت نماید
@@ -29,3 +29,49 @@ if not a == b:
 # first number is greater than second number but is lower than third one.
 # اگر عدد اول از عدد دوم کوچکتر ولی از عدد سوم بزرگتر باشد، پیغام زیر را نمایش دهد
 # first number is greater than third number but is lower than second one.
+
+
+# age = 15
+# if age == 13 or age == 14 or 15 <= age < 19:
+#     print("you are teenager!")
+
+# if age >= 13 and age < 19:
+#     print("you are teenager!")
+# if 13 <= age < 19:
+#     print("you are teenager!")
+# سن و نام فردی را از ورودی بگیرید و
+# در صورتی که سن او کمتر از 13 سال بود
+# kid
+# اگر بین 13 تا 19 بود
+# teenager
+# اگر بین 19 تا 22 بود
+# junior
+# در غیر اینصورت
+# adult
+
+# a = True
+# if a:
+#     print("this is True")
+# if not (a):
+#     print("nothing!")
+
+
+# a = False
+# if a:
+#     print("this is True")
+# if not (a):
+#     print("nothing!")
+
+# Exercise:
+a = True
+b = False
+if a and b:
+    print("a and b are both True.")
+
+a = True
+b = True
+
+if a and b:
+    print("ok")
+if a or b:
+    print("ok")
