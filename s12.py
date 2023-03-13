71ac008d90d02764cd75e5d3128f206d690bca89
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-13 17:59:15 +0330

    sadeghi

---
+++ s12.py
@@ -0,0 +1,56 @@
+# USERNAME = 'admin'
+# PASS = 'root'
+
+
+# user_name = input('enter user name:> ')
+# password = input('enter password:> ')
+
+# if user_name == USERNAME and password == PASS:
+#     print('user name and password is valid.')
+#     print('login was successful')
+# else:
+#     print("user name or password is not correct.")
+#     print('login failed...')
+
+
+a = 4
+b = 5
+c = 6
+
+
+# if a < b:
+#     print("a is less than b")
+# if a > b:
+#     print("a is greater than than b")
+# if a <= b:
+#     print("a is less than or equal to b")
+# if a >= b:
+#     print("a is greater than or equal to b")
+
+a = 4
+b = 5
+c = 6
+
+# if a < b and a < c:
+#     print("a is less than b and c")
+
+# if a < b or a < c:
+#     print("a is less than either a or b (or both)")
+
+# a = True
+# if a:
+#     print("a is true")
+# if not a:
+#     print("a is false")
+
+
+a = True
+b = False
+if a and b:
+    print("salaam")
+
+
+a = 3
+b = 3
+c = a == b
+print(c)
