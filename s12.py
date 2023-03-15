213e4971573b18d38726408b5f753f3d65759da9
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-15 17:58:27 +0330

    sadeghi

--- s12.py
+++ s12.py
@@ -44,13 +44,47 @@ c = 6
 #     print("a is false")
 
 
-a = True
-b = False
-if a and b:
-    print("salaam")
+# a = True
+# b = False
+# if a and b:
+#     print("salaam")
 
 
-a = 3
-b = 3
-c = a == b
-print(c)
+# a = 3
+# b = 3
+# c = a == b
+# print(c)
+
+
+# if 1:
+#  print("1")
+
+# if 0:
+#  print('0')
+
+# if "A":
+#  print("A")
+
+# if "":
+#  print("some thing")
+
+USERNAME = ['admin', 'root']
+PASSW = 'root'
+
+user_name = input('enter user name:> ')
+password = input('enter password:> ')
+
+if user_name and password:
+    if user_name == USERNAME and password == PASSW:
+        print("account is valid")
+        print("login was successful")
+    else:
+        print("user name or password is not correct")
+
+else:
+    print('you must enter user name and password')
+
+
+# برنامه با لا را به گونه ای اصلاح کنید که هر دو نام کاربری را بتوان وارد نمود
+# یعنی اگر نام کاربری اول یا نام کاربری دوم را وارد نماید
+# و کلمه عبور را نیز وارد نماید، لاگین موفقیت آمیز باشد
