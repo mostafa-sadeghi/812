785cfb05eea39509a59742b1e30dbafd99184162
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-19 17:59:28 +0330

    sadeghi

---
+++ s19.py
@@ -0,0 +1,16 @@
+i = 0
+total = 0
+
+while i < 5:
+    n = int(input('enter a number:> '))
+    total = total + n
+    i = i + 1
+
+print(total)
+
+for i in range(5):
+    n = int(input('enter a number:> '))
+    total = total + n
+print(total)
+
+# exercise : کشیدن مربع و پنج ضلعی با استفاده از حلقه وایل
